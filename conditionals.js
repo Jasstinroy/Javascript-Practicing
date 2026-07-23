fc85ee98cbd3bdd2c9cf99b7c8c5cb5b0710e86e
Jasstin Roy <sy2373570@gmail.com>
2026-07-23 13:43:56 +0530

    conditionals commit

---
+++ conditionals.js
@@ -0,0 +1,16 @@
+age=2;
+if(age>=18){
+    console.log("You are eligible to vote");
+} else{
+    console.log("You are not eligible to vote");
+}
+rating=4;
+if(rating==5) console.log("Excellent");
+else if(rating==4) console.log("Good");
+else if(rating==3) console.log("Average");
+else if(rating==2) console.log("Poor");
+else console.log("Very Poor");
+
+x=4567;
+if(x>=100 && x<=999)console.log("The number is between 100 and 9999");
+ else console.log("The number is not between 100 and 9999");
