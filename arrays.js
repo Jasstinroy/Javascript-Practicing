931e8e78dcdfb3d0fc52fcbcceca9105c36d7ba4
Jasstin Roy <sy2373570@gmail.com>
2026-07-24 11:52:31 +0530

    array commit

---
+++ arrays.js
@@ -0,0 +1,16 @@
+arr=[1,6,8,3];
+console.log(arr);
+n=arr.length;
+// for(let i=0;i<n;i++){
+//     console.log(arr[i]);
+// }
+arr.push(10);
+console.log(arr);
+arr.push(10);//push at last
+console.log(arr);
+arr.pop(10);//remove last element
+console.log(arr);
+arr.unshift(0);//add at first
+console.log(arr);
+arr.shift();//remove first element
+console.log(arr);
---
+++ arrays2.js
@@ -0,0 +1,27 @@
+ let arr=[2,8,3,3];
+ arr[0]=1;
+ console.log(arr);
+
+  const arr1=[1,2,3,4];
+  arr1[0]=10;
+  console.log(arr1);
+  //arr1=2.4;
+  //console.log(arr1);
+
+  arr2=[4,"raghav",true,7.9];
+  for(let i=0;i<arr2.length;i++){
+    console.log(i,arr2[i]);
+  }
+  console.log(arr2);
+
+  brr=[[1,2,2,3],[4,5,6,7]];
+  brr1=[[1,2],[4,5,6,7]];
+  console.log(brr);
+  console.log(brr1);
+  console.log(brr[0][2]);
+  console.log(brr[1][3]);
+  for
+
+
+
+
