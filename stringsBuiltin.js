5808fd6dd0932ef2f4ad2098cf755c619e01762f
Jasstin Roy <sy2373570@gmail.com>
2026-07-24 16:54:23 +0530

    stringsBuiltin commit

---
+++ stringsBuiltin.js
@@ -0,0 +1,18 @@
+let s="  raghav garg  ";
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.length);
+ let s2=s.trim();// bich k spaces nahi hatte hai . 
+ console.log(s2,s2.length);
+ console.log(s2.indexOf('a'));
+ console.log(s2.lastIndexOf('a'));
+ console.log(s2.charAt(3));
+ console.log(s2[3]);
+
+ let v="raghavgarg";
+ console.log(v.slice(2));
+ console.log(v.slice(2,5));
+  let a= "raghav garg is a teacher at physics wallah";
+  console.log(a);
+  let arr=s.split('');
+  console.log(arr);
