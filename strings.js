638d0ae76a9acd5bc972a97b25cce245b75cd150
Jasstin Roy <sy2373570@gmail.com>
2026-07-24 16:37:31 +0530

    strings commit

---
+++ strings.js
@@ -0,0 +1,9 @@
+let s="Coders";
+console.log(s,s.length);
+for(let i=0;i<s.length;i++){
+    console.log(s[i]);
+}
+for(const char of s){
+    console.log(char);
+}
+// for each loop does not work for strings
