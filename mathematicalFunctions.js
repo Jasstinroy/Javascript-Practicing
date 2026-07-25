2473b977d0410c7bbace3432104af6e935841ea8
Jasstin Roy <sy2373570@gmail.com>
2026-07-25 13:57:56 +0530

    Mathematical functions commit

---
+++ mathematicalFunctions.js
@@ -0,0 +1,10 @@
+console.log(Math.abs(-7));
+console.log(Math.max(5,1));
+console.log(Math.min(5,1));
+console.log(Math.pow(2,3));
+console.log(Math.sqrt(16));
+console.log(Math.cbrt(27));
+console.log(Math.log(2));
+console.log(Math.log10(100));
+console.log(Math.floor(5.9));//niche walla number dega
+console.log(Math.random());
