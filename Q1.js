513acefe6dcb784b458bded6803e5ec1d2342de8
Jasstin Roy <sy2373570@gmail.com>
2026-07-25 20:53:14 +0530

    Q1 commit

---
+++ Q1.js
@@ -0,0 +1,11 @@
+//write code to generate a random number between 0 and 9 (included)
+let randomNumber = Math.floor(Math.random() * 10);
+console.log(randomNumber);
+
+//0 to 10
+let randomno=Math.floor(Math.random()*11);
+console.log(randomno);
+
+//-10 to 10
+let randomnumber1=Math.floor(Math.random()*12)-1;
+console.log(randomnumber1);
